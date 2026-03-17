5920130956c2e5f4007f502aeda665ccabcf3ee5
Zamchik <zamchik28@mail.ru>
2026-03-17 22:04:30 +0500

    Introduction redact 5

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -4,21 +4,24 @@ import image from '../../assets/I_am.jpg';
 const Introduction = () => {
     return (
         <section className={styles.page_top}>
-            <div className={styles.top_wrap}>
-                <picture className={styles.i_am}>
-                    <img src={image} alt="I_am" className={styles.i_am_photo} />
-                    <div className={styles.i_am_shadow}></div>
-                </picture>
-                <div className={styles.top_text}>
-                    <h1 className={styles.my_name}>
-                        <span className={styles.my_name_title}>Konstantin</span>
-                        <span className={styles.my_name_title}>Zamyakin</span>
-                    </h1>
-                    <h2>
-                        <span>Frontend Developer</span>
-                    </h2>
+            <div className={styles.custom_container}>
+                <div className={styles.top_wrap}>
+                    <picture className={styles.i_am}>
+                        <img src={image} alt="I_am" className={styles.i_am_photo} />
+                        <div className={styles.i_am_shadow}></div>
+                    </picture>
+                    <div className={styles.top_text}>
+                        <h1 className={styles.my_name}>
+                            <span className={styles.my_name_title}>Konstantin</span>
+                            <span className={styles.my_name_title}>Zamyakin</span>
+                        </h1>
+                        <h2>
+                            <span>Frontend Developer</span>
+                        </h2>
+                    </div>
                 </div>
             </div>
+
         </section>
     )
 }
